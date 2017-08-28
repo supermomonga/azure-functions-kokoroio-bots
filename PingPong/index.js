3d9d821700afe686ffaa6f3cf46ccf9ed2ed7b47
supermomonga <hi@supermomonga.com>
2017-08-28 09:24:12 +0900

    npm packages

--- PingPong/index.js
+++ PingPong/index.js
@@ -1,3 +1,5 @@
+var request = require('request');
+
 module.exports = function (context, req) {
     var callbackSecret = getEnvironmentVariable("KokoroIoCallbackSecret");
     var accessToken = getEnvironmentVariable("KokoroIoAccessToken");
@@ -6,31 +8,49 @@ module.exports = function (context, req) {
 
     context.done();
 
-    if(!callbackSecret)
-        return context.log("Please configure KokoroIoCallbackSecret.");
+    if(!callbackSecret) {
+        context.log("Please configure KokoroIoCallbackSecret.");
+        context.res = { status: 401, body: "Please configure KokoroIoCallbackSecret." };
+        return context.done();
+    }
 
-    if(!authorization)
-        return context.log("Please pass an Authorization HTTP request header.");
+    if(!authorization) {
+        context.log("Please pass an Authorization HTTP request header.");
+        context.res = { status: 401, body: "Please pass an Authorization HTTP request header." };
+        return context.done();
+    }
 
-    if(authorization != callbackSecret)
-        return context.log("Invalid Authorization HTTP request header.");
+    if(authorization != callbackSecret) {
+        context.log("Invalid Authorization HTTP request header.");
+        context.res = { status: 401, body: "Invalid Authorization HTTP request header." };
+        return context.done();
+    }
 
     var data = req.body;
     var roomId = data.room.id;
     var message = data.raw_content;
     var userScreenName = data.profile.screen_name;
 
-    if(!message)
-        return context.log("Invalid MessageEntity data structore.");
+    if(!message) {
+        context.log("Invalid MessageEntity data structore.");
+        context.res = { status: 401, body: "Invalid MessageEntity data structore." };
+        return context.done();
+    }
 
-    if(message != "ping")
+    if(message != "ping") {
+        context.res = { status: 200, body: "Not a ping." };
         return;
+    }
 
     var responseMessage = `@${ userScreenName } pong`;
     var url = `${ baseUrl }api/v1/bot/rooms/${ roomId }/messages`;
     request(url, (error, response, body) => {
-        if(error) return context.log("Invalid Authorization HTTP request header.");
+        if(error) {
+            context.log("Invalid Authorization HTTP request header.");
+            context.res = { status: 401, body: "Invalid Authorization HTTP request header." };
+        }
     });
+    return context.done();
 };
 
 function getEnvironmentVariable(name) {
